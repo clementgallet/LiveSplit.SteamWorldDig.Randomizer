e232e863cf60a14087592850cf6f09a12e117d8c
Clément Gallet <clement.gallet@ens-lyon.org>
2017-12-05 13:31:31 +0100

    Fix version
     This should make livesplit update to work again

--- SteamWorldFactory.cs
+++ SteamWorldFactory.cs
@@ -4,13 +4,13 @@ using System;
 using System.Reflection;
 namespace LiveSplit.SteamWorldDig {
 	public class SteamWorldFactory : IComponentFactory {
-		public string ComponentName { get { return "SteamWorld Dig Randomizer v" + this.Version.ToString(); } }
+		public string ComponentName { get { return "SteamWorld Dig Randomizer"; } }
 		public string Description { get { return "Randomizer for SteamWorld Dig"; } }
 		public ComponentCategory Category { get { return ComponentCategory.Control; } }
 		public IComponent Create(LiveSplitState state) { return new SteamWorldComponent(); }
 		public string UpdateName { get { return this.ComponentName; } }
 		public string UpdateURL { get { return "https://raw.githubusercontent.com/clementgallet/LiveSplit.SteamWorldDig.Randomizer/master/"; } }
 		public string XMLURL { get { return this.UpdateURL + "Components/LiveSplit.SteamWorldDig.Randomizer.Updates.xml"; } }
-		public Version Version { get { return Assembly.GetExecutingAssembly().GetName().Version; } }
+		public Version Version { get { return Version.Parse("1.2"); } }
 	}
 }
